3bd0da76036a21ec1ffac2a5cbf4edb31edeb8f0
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-09 17:52:31 -0500

    feat: Add player_computer function for computer input in guess a number game #7

--- play2.py
+++ play2.py
@@ -27,3 +27,12 @@ def player_user():
     """The function allows the user to enter a number"""
 
     number_user.append(int(input("\nIntroduce un número: ")))
+
+def player_computer(number_random):
+    """The function allows the computer to enter a number"""
+
+    if number_user[-1] > number_random:
+        number_computer.append(random.randint(1, number_user[-1] - 1))
+    else:
+        number_computer.append(random.randint(number_user[-1] + 1, 100))
+    print("El ordenador ha elegido el numero: ", number_computer[-1])
