48079f7ff3818854f8295df3e890d8d1ff198eac
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-09 17:49:54 -0500

    feat: Add number validation and comparison logic function validate_number #7

---
+++ play2.py
@@ -0,0 +1,24 @@
+"""
+randint return a random number selected element from the specified range
+random.randint(start, end)
+start Required. An integer specific at which position to start.
+end Required. An integer specific at which position to end.
+
+"""
+
+import random
+
+number_user = []
+number_computer = []
+MAX_TRY = 10
+
+def validate_number(number, number_random):
+    """The function validates the number entered by the user
+    and compares it with the random number"""
+
+    if number == number_random:
+        print("¡Felicidades! Has adivinado el número")
+    if number < number_random:
+        print("El número a adivinar es mayor")
+    else:
+        print("El número a adivinar es menor")
