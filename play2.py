da8ac74db36961515aafff45711bd08c850f4190
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-09 17:55:19 -0500

    feat: Add play_again function for replaying the game #7

--- play2.py
+++ play2.py
@@ -67,3 +67,16 @@ def game():
     # .map() function returns a list of the results converted to string
     print("numeros introducidos por el usuario:", ' '.join(map(str, number_user)))
     print("numeros introducido por el ordenador:",' '.join(map(str, number_computer)))
+
+game()
+
+def play_again():
+    """The function allows the user to play again the game"""
+
+    play_again_user = input("¿Quieres jugar de nuevo? (s/n): ")
+    if play_again_user == "s":
+        game()
+    else:
+        print("¡Gracias por jugar!")
+
+play_again()
