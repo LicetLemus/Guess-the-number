b1ba787f0c37f33dfb1f01f3ac69d12701babc2a
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-12 15:58:00 -0500

    chore: Add of test_players.py for better function player_user and player_computer

--- tests/test_players.py
+++ tests/test_players.py
@@ -1,16 +1,41 @@
-import unittest # importar la librería unittest para realizar las pruebas
-from unittest.mock import patch # esta funcion permite simular o mockear objetos dentro de las pruebas
-from src.players import player_user, number_user
+import unittest
+from unittest.mock import patch # los patch se utilizan para simular la entrada de datos, sustituyendo la función original temporalmente mientras se ejecutan las pruebas
+from src.players import player_user, player_computer
 
-# unittest trabaja con una clase que hereda de TestCase, por lo tanto, se crea una clase TestPlayers que hereda de 
 class TestPlayers(unittest.TestCase):
     """
-    Test the functions of the players module
+    Test the functions of the players module.
     """
-    @patch('builtins.input', return_value='5') # se simula la entrada de un valor por teclado con el valor 5
-    def test_player_user(self, mock_input): # self es el objeto que se pasa a la función
+
+    @patch('src.players.number_user', new_callable=list)
+    @patch('builtins.input', return_value=5)
+    def test_player_user(self, mock_input, mock_number_user):
         """
-        Test the player_user function
+        Test the player_user function.
         """
-        player_user() # se llama a la función player_user
-        self.assertEqual(number_user[-1], 5) # se comprueba que el último valor de la lista number_user sea 5
+        player_user()  # Llama a la función player_user
+        self.assertEqual(mock_number_user[-1], 5)  # Verifica que el valor de mock_number_user[-1] sea igual a 5
+
+
+    @patch('src.players.number_user', new_callable=list)
+    @patch('src.players.number_computer', new_callable=list)
+    @patch('random.randint', return_value=3)
+    def test_player_computer_with_number_random_smaller(self, mock_randint, mock_number_computer, mock_number_user):
+        """
+        Test the player_computer function when the random number is smaller than the user number.
+        """
+        mock_number_user.append(10)  # Simula que el usuario ya ha introducido el número 10
+        player_computer(9)  # Llama a la función player_computer con un número aleatorio menor (9)
+        self.assertEqual(mock_number_computer[-1], 3)  # Verifica que el número elegido por el ordenador sea 3
+
+
+    @patch('src.players.number_user', new_callable=list) # simula la lista de números introducidos por el usuario
+    @patch('src.players.number_computer', new_callable=list) # simula la lista de números introducidos por el ordenador
+    @patch('random.randint', return_value=15)
+    def test_player_computer_with_number_random_greater(self, mock_randint, mock_number_computer, mock_number_user):
+        """
+        Test the player_computer function when the random number is greater than the user number.
+        """
+        mock_number_user.append(10)  # Simula que el usuario ya ha introducido el número 10
+        player_computer(11)  # Llama a la función player_computer con un número aleatorio mayor (11)
+        self.assertEqual(mock_number_computer[-1], 15)  # Verifica que el número elegido por el ordenador sea 15
