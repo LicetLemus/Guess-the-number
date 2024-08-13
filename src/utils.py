c3f7b1b527e9afba5dcd28b36649538cec62a1a5
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-13 16:57:04 -0500

    feat: Update get_validate_number function in utils.py to use ranges instead of range_limit

--- src/utils.py
+++ src/utils.py
@@ -1,34 +1,40 @@
 import random
 
 def print_information():
+    """
+    The function prints a welcome message
+    """
     print("Bienvenido al juego de adivinar el número, ¡Buena suerte!")
     print("¡se ha generado un número aleatorio entre 1 y 100!")
 
-def get_random_number(range_limit):
+def get_random_number(ranges):
     """"
-    The function generates a random number between 1 and 100.
+    The function generates a random number.
     
     Args:
         range_limit (tuple): A tuple with the range of numbers to generate the random number.
         
     return : int: The random number generated.
     """
-    a, b = range_limit
-    number_random = random.randint(a, b)
+    min = ranges["min"]
+    max = ranges["max"]
+    number_random = random.randint(min, max)
     return number_random
 
-def get_validate_number(number, number_random, range_limit, entity=""):
+def get_validate_number(number, number_random, ranges, entity=""):
     """The function validates the number entered by the user or the computer.  
     Args:
         number (int): The number to validate.
         number_random (int): The number to compare against.
+        range_limit (tuple): A tuple with the range of numbers to validate the number.
         entity (str): A description of who is guessing (e.g., "usuario" or "ordenador").
     
-    Returns:
+    Return:
         bool: True if the number is correct, False otherwise.
         str: A message indicating if the number is correct or not.
     """
-    if not number or number < range_limit[0] or number > range_limit[1]:
+    print("number------------------------------------", number)
+    if number < ranges['min'] or number > ranges['max']:
         return False, f"{entity}: El número no es válido"
     elif number == number_random:
         return True, f"¡Ganaste {entity}!, adivinaste el número"
@@ -36,5 +42,3 @@ def get_validate_number(number, number_random, range_limit, entity=""):
         return False, f"{entity}: El número a adivinar es mayor"
     else:
         return False, f"{entity}: El número a adivinar es menor"
-
-    
