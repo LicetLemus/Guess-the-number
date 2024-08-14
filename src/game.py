b369db812d9f423baabe43f642802bbdfa51a247
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-14 11:19:43 -0500

    feat: Refactor game.py to improve user and computer number handling without depending of variable global

--- src/game.py
+++ src/game.py
@@ -16,22 +16,29 @@ def game():
     number_random = get_random_number(ranges)
     print_information()
 
-    try_done_round = 0
-    message_feedback = [""]
+    numbers_user_computer = []  # Lista para almacenar los números introducidos por ambos
+    feedback_list = [""]
     
     while try_done_round < MAX_TRY:
-        number_user = set_number_user()
-        number_computer = set_number_computer(message_feedback[-1])
+        number_user, numbers_user_computer = set_number_user(numbers_user_computer)
+        if number_user is None:
+            continue  # Si el número no es válido, pedir al usuario que intente de nuevo
+        
+        
+        # Obtener el número de la computadora
+        number_computer, numbers_user_computer = set_number_computer(numbers_user_computer, feedback_list[-1], ranges)
+        
+        #mostrar los numeros de los jugadores
         print_number_user_computer(number_user, number_computer)
         
-        is_correct_user, message_user = get_validate_number(number_user[-1], number_random, ranges, "usuario")
-        message_feedback.append(message_user)
+        is_correct_user, message_user = get_validate_number(number_user, number_random, ranges, "usuario")
+        feedback_list.append(message_user)
         print(message_user)
         if is_correct_user:
             print("¡Juego terminado!")
             break
         
-        is_correct_computer, message_computer = get_validate_number(number_computer[-1], number_random, ranges, "computadora")
+        is_correct_computer, message_computer = get_validate_number(number_computer, number_random, ranges, "computadora")
         print(message_computer)
         if is_correct_computer:
             print("¡Juego terminado!")
@@ -39,14 +46,16 @@ def game():
         
         try_done_round += 1
 
-        if try_done_round == 10:
+        if try_done_round == MAX_TRY:
             print("¡Has agotado tus intentos!")
             break
+        
         print("Intentos restantes: ", MAX_TRY - try_done_round)
     print("\nEl número a adivinar era: ", number_random)
 
     # .join () method returns a string in which the elements of sequence have been
     # joined by str separator ' '.
     # .map() function returns a list of the results converted to string
-    print("numeros introducidos por el usuario:", ' '.join(map(str, number_user)))
-    print("numeros introducido por el ordenador:",' '.join(map(str, number_computer)))
+    print("numeros introducidos por el usuario:", ' '.join(map(str, numbers_user_computer[:len(numbers_user_computer)//2]))) # Imprimir los números introducidos por el usuario
+
+    print("numeros introducido por el ordenador:",' '.join(map(str, numbers_user_computer[len(numbers_user_computer)//2:]))) # Imprimir los números introducidos por el ordenador
